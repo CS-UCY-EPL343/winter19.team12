3b4acf80f7684fd8d4e745195b21ea42da6ce6d2
apanay20 <55096402+apanay20@users.noreply.github.com>
2019-11-18 19:53:14 +0200

    Register Page Changes

--- fitbit_frontend/src/i18n/en-us/index.js
+++ fitbit_frontend/src/i18n/en-us/index.js
@@ -6,5 +6,9 @@ export default {
   username_hint: 'Enter your account\'s username.',
   password: 'Password',
   password_hint: 'Enter your account\'s password.',
+  retype_password: 'Retype Password',
+  retype_password_hint: 'Retype your account\'s password.',
+  serial_number: 'Serial Number',
+  serial_number_hint: 'Enter your Fitbit device S/N.',
   field_required: 'This field is required.'
 }
--- fitbit_frontend/src/pages/Register.vue
+++ fitbit_frontend/src/pages/Register.vue
@@ -44,10 +44,10 @@
             <q-icon name='vpn_key' />
           </template>
         </q-input>
-        <q-input :label="$t('Retype Password')"
-                 :hint="$t('password_hint')"
+        <q-input :label="$t('retype_password')"
+                 :hint="$t('retype_password_hint')"
                  type='password'
-                 v-model='password'
+                 v-model='retype_password'
                  ref='retype_password'
                  spellcheck='false'
                  outlined bottom-slots
@@ -56,14 +56,28 @@
             <q-icon name='vpn_key' />
           </template>
         </q-input>
-        <template>
-           <div class='row full-width q-col-gutter-sm'>
-              <div class="q-gutter-sm">
-                 <q-radio dense v-model="shape" val="user_select" label="Fitbit User" />
-                 <q-radio dense v-model="shape" val="specialist-select" label="Specialist" />
-              </div>
-           </div>
-    </template>
+       <div class='row full-width q-col-gutter-sm'>
+          <div class="q-gutter-sm">
+             <q-radio dense v-model="type" val="user_select" label="Fitbit User" />
+             <q-radio dense v-model="type" val="specialist_select" label="Specialist" />
+          </div>
+       </div>
+       <q-slide-transition>
+       <div v-show="type === 'user_select'">
+       <q-input :label="$t('serial_number')"
+                 :hint="$t('serial_number_hint')"
+                 type='serial_number'
+                 v-model='serial_number'
+                 ref='serial_number'
+                 spellcheck='false'
+                 outlined bottom-slots
+        >
+          <template v-slot:prepend>
+            <q-icon name='watch' />
+          </template>
+        </q-input>   
+       </div>
+       </q-slide-transition>
       </q-card-section>
       <q-separator />
       <q-card-actions class='bg-grey-1 q-pl-none'>
@@ -84,7 +98,31 @@
 
 <script>
 export default {
-  name: 'Register'
+  name: 'Register',
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      retype_password: '',
+      type: 'user_select',
+      serial_number: ''
+    }
+  },
+  methods: {
+    submit () {
+      if (this.username === '' || this.password === '' || this.email === '' || this.retype_password === '') {
+        this.$q.notify('Please fill in all the fields in the form.')
+      } else if (this.password !== this.retype_password) {
+        this.$q.notify('Passwords not match.')
+      } else if (this.type === 'user_select' && this.serial_number === '') {
+        this.$q.notify('Please enter Fitbit Device Serial Number.')
+      } else {
+        this.$q.notify(`${this.username} submitted the form.`)
+      }
+      console.log(this.$route)
+    }
+  }
 }
 
 </script>
