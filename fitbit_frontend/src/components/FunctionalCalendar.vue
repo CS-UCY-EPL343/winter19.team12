4033a2e3f43129cc41c36c9fc23f52fd7ed0d3c0
lkrist01 <kristilici@gmail.com>
2020-02-03 09:43:19 +0000

    Bug fixed in Calendar

--- fitbit_frontend/src/components/FunctionalCalendar.vue
+++ fitbit_frontend/src/components/FunctionalCalendar.vue
@@ -1,16 +1,12 @@
 <template>
   <div >
     <p>Please select the day to present metrics</p>
-    {{new Date().getDate() }} {{new Date().getMonth() }}
-
     <functional-calendar  :is-modal='true' :is-date-range='true'></functional-calendar>
   </div>
 </template>
 
 
 <script>
-  var a = new Date().getDate()+":"+new Date().getMonth()+"apoel"
-  console.log(a)
   // Introduced in vue file
   import { FunctionalCalendar } from 'vue-functional-calendar';
 
