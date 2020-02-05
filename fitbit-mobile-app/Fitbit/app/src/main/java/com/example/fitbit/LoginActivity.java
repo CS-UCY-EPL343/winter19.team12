df344a23805a5aab10d053235ebc3e6a6113f47d
cconst04 <cconst04@ucy.ac.cy>
2020-02-05 18:01:53 +0200

    Android login using JWT

--- fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/LoginActivity.java
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/LoginActivity.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoginActivity extends Activity{
-    public static final String LOGIN_ENDPOINT="/login_api";
+    public static final String LOGIN_ENDPOINT="/get_token";
     private Button mLoginBtn;
     private boolean loginSuccess=false;
     private EditText mPasswordInput,mUsernameInput;
@@ -51,8 +51,9 @@ public class LoginActivity extends Activity{
                     .show();
             return;
         }
-        String params = String.format("{'username':'%s','password':'%s'}",username,password);
-        CallAPI request = new CallAPI(new AsyncResponse() {
+        User.getUser().setUsername(username);//sets the username in the singleton User class
+        String params = String.format("{\"username\":\"%s\",\"password\":\"%s\"}",username,password);
+        CallAPI request = new CallAPI("GET",new AsyncResponse() {
             @Override
             public void processFinish(String output) {
                 Log.d("TEST","output:"+output);
@@ -60,7 +61,7 @@ public class LoginActivity extends Activity{
                     JSONObject obj = new JSONObject(output);
                     String title=null;
                     String message = null;
-                    if(obj.getString("status").equals("0")){
+                    if(!obj.has("access")){
                         title="Wrong credentials";
                         message="Wrong username or password entered";
                         new AlertDialog.Builder(LoginActivity.this)
@@ -68,9 +69,9 @@ public class LoginActivity extends Activity{
                                 .setMessage(message)
                                 .show();
                     }
-                    else if (obj.getString("status").equals("1")){
-                        title="Login success";
-                        message = "Correct credentials";
+                    else{
+
+                        User.getUser().setJWT(obj.getString("access"));
                         Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(myIntent);
                     }
