8deccaec5b74a4e23f7afa8666d42c2f1762789e
cconst04 <cconst04@ucy.ac.cy>
2020-02-06 20:23:18 +0200

    Android:Added Nano server and internet connectivity change listener

---
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/CollectDataService.java
@@ -0,0 +1,30 @@
+package com.example.fitbit;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.app.Service;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class CollectDataService extends Service {
+    private Nano mNano;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // do your jobs here
+        Log.d("service","started service");
+        try {
+            mNano = new Nano(80);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
---
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/NetworkChangeReceiver.java
@@ -0,0 +1,25 @@
+package com.example.fitbit;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    private static boolean isConnected;
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+        int status = NetworkUtil.getConnectivityStatusString(context);
+        if ("android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())) {
+            if (status == NetworkUtil.NETWORK_STATUS_NOT_CONNECTED) {
+                Log.d("conn","not connected");
+                isConnected=false;
+            } else {
+                Log.d("conn","connected");
+                isConnected=true;
+            }
+        }
+    }
+}
