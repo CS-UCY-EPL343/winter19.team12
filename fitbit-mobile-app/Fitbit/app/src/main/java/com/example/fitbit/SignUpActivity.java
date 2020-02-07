aa522895d3024674565cfc79bbbe68b941e46a38
parisconstantinides <pkonst06@cs.ucy.ac.cy>
2020-02-07 23:52:27 +0200

    Android Create Profile #4 - Fix the view of the activity added the logic

--- fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/SignUpActivity.java
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/SignUpActivity.java
@@ -1,126 +1,74 @@
 package com.example.fitbit;
 
-import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SignUpActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
+    private static final String SIGNUPENDPOINT="/register_api";
 
-    @InjectView(R.id.input_name) EditText _nameText;
-    @InjectView(R.id.input_email) EditText _emailText;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_signup) Button _signupButton;
-    @InjectView(R.id.link_login) TextView _loginLink;
+    EditText username;
+    EditText password;
+    EditText rePassword;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        ButterKnife.inject(this);
-
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signup();
+        username=findViewById(R.id.editTextUsernameSignUp);
+        password=findViewById(R.id.editTextPasswordSignUp);
+        rePassword=findViewById(R.id.editTextRePasswordSignUp);
+        findViewById(R.id.buttonSignUp).setOnClickListener((view)->{
+            if(username.getText().toString().trim().equals("")){
+                username.setError("You must provide a username");
+                return;
+            }else if(password.getText().toString().equals("")){
+                password.setError("You must provide a password");
+                return;
+            }else if(!password.getText().toString().equals(rePassword.getText().toString())){
+                rePassword.setError("Password are not matching");
+                return;
             }
-        });
-
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });
-    }
-
-    public void signup() {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
-        }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
-                R.style.Base_Theme_AppCompat);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
+            CallAPI request=new CallAPI("POST", output -> {
+                try {
+                    JSONObject obj = new JSONObject(output);
+                    if(obj.has("error")){
+                        if(obj.getString("error").equals("username already exists")){
+                            username.setError("Username already exists");
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this,obj.getString("error"),Toast.LENGTH_SHORT).show();
+                        }
+                    }else if(obj.has("status")){
+                        Intent myIntent = new Intent(SignUpActivity.this, MainActivity.class);
+                        startActivity(myIntent);
                     }
-                }, 3000);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            });
+            String params = String.format("{\"username\":\"%s\",\"password\":\"%s\",\"repeat_password\":\"%s\"}",username.getText().toString(),password.getText().toString(),rePassword.getText().toString());
+
+            request.execute(Urls.SERVER_URL+SIGNUPENDPOINT,params);
+        });
+
+        findViewById(R.id.textViewLogin).setOnClickListener((view)->{
+            Intent myIntent= new Intent(SignUpActivity.this,LoginActivity.class);
+            startActivity(myIntent);
+        });
+
     }
 
 
-    public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
-
-    public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _signupButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-//        if (name.isEmpty() || name.length() < 3) {
-//            _nameText.setError("at least 3 characters");
-//            valid = false;
-//        } else {
-//            _nameText.setError(null);
-//        }
-//
-//        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//            _emailText.setError("enter a valid email address");
-//            valid = false;
-//        } else {
-//            _emailText.setError(null);
-//        }
-//
-//        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-//            _passwordText.setError("between 4 and 10 alphanumeric characters");
-//            valid = false;
-//        } else {
-//            _passwordText.setError(null);
-//        }
-
-        return valid;
-    }
 }
---
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/Urls.java
@@ -0,0 +1,5 @@
+package com.example.fitbit;
+
+public class Urls {
+    public static final String SERVER_URL="http://localhost:80";
+}
