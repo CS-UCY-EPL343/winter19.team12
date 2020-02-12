616836c3892d2d63af791d55601f3eca7e7d0f28
parisconstantinides <pkonst06@cs.ucy.ac.cy>
2020-02-12 12:45:32 +0200

    Android Edit Profile #8- Created the view and design

--- fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/EditProfile.java
+++ fitbit-mobile-app/Fitbit/app/src/main/java/com/example/fitbit/EditProfile.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import com.example.fitbit.model.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class EditProfile extends AppCompatActivity {
     private final String EDIT_PROFILE_ENDPOINT="/edit_profile_api";
+    private final String VIEW_PROFILE_DETAILS="/get_user_info";
     private EditText password;
     private EditText rePassword;
     private EditText name;
@@ -36,5 +42,24 @@ public class EditProfile extends AppCompatActivity {
         email=findViewById(R.id.editTextEmailEditProfile);
         phone=findViewById(R.id.editTextTelephoneEditProfile);
         address=findViewById(R.id.editTextAddressEditProfile);
+
+        CallAPI userInfo=new CallAPI("POST",(r)->{
+            try {
+                JSONObject results=new JSONObject(r);
+                height.setText(results.getString("height"));
+                birthday.setText(results.getString("birthdate"));
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
+        User currentUser=User.findById(User.class,1);
+        userInfo.execute(Urls.SERVER_URL+VIEW_PROFILE_DETAILS,currentUser.getUsername());
+
+        findViewById(R.id.buttonEditProfileSubmit).setOnClickListener((l)->{
+
+        });
     }
 }
