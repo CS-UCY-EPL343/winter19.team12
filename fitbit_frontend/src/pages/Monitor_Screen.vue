ba26a9274b4c24d8d59a5ae9012464a4d6092ef1
lkrist01 <kristilici@gmail.com>
2020-02-29 12:05:44 +0000

    Adding features to monitor screen

--- fitbit_frontend/src/pages/Monitor_Screen.vue
+++ fitbit_frontend/src/pages/Monitor_Screen.vue
@@ -5,15 +5,66 @@
       :data="data"
       :columns="columns"
       row-key="name"
-    />
+      :filter="filter"
+    >
+
+      <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th auto-width />
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td auto-width>
+            <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'remove' : 'add'" />
+          </q-td>
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+        <q-tr v-show="props.expand" :props="props">
+          <q-td colspan="100%">
+            <div class="text-left">
+              <q-btn color="primary" label="Select patient" class="q-mt-md">
+                <q-tooltip content-class="bg-accent">See {{ props.row.name }} data </q-tooltip>
+              </q-btn>
+            </div>
+          </q-td>
+        </q-tr>
+      </template>
+
+    </q-table>
   </div>
 </template>
+
+
 
 
 <script>
 export default {
   data () {
     return {
+      filter: '',
       columns: [
         {
           name: 'name',
@@ -25,23 +76,19 @@ export default {
           sortable: true
         },
         { name: 'Register date', align: 'center', label: 'Register date', field: 'date', sortable: true },
-        { name: 'protein', label: 'Protein (g)', field: 'protein' },
-        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        { name: 'age', label: 'Age', field: 'age' },
 
       ],
       data: [
         {
           name: 'Mark',
           date: '11-11-2020',
-          protein: 4.0,
-          sodium: 87,
-
+          age: 23,
         },
         {
           name: 'Ana',
           date: '10-02-2020',
-          protein: 4.3,
-          sodium: 129,
+          age: 45,
         }
       ]
     }
